aadb0e95faf2e55c0aeb5e9140b586a7e5934c30
andmohiko <thomasito.berryz@gmail.com>
2019-10-23 17:29:02 +0900

    add markdownit

---
+++ plugins/markdown-it.js
@@ -0,0 +1,17 @@
+import MarkdownIt from 'markdown-it'
+
+export default ({ app }, inject) => {
+  const md = new MarkdownIt({
+    langPrefix: 'language-',
+    preset: 'default',
+    linkify: false,
+    breaks: true,
+    html: true,
+    typegraphy: true,
+  })
+
+  md.use(require('markdown-it-meta'))
+  md.use(require('markdown-it-anchor'))
+
+  inject('md', md)
+}
