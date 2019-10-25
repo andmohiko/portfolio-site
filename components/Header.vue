e43a6dd2b4728844867f1cd3633f6d27eb5c9d3e
andmohiko <thomasito.berryz@gmail.com>
2019-10-25 09:32:08 +0900

    fix nav bg color

--- components/Header.vue
+++ components/Header.vue
@@ -26,7 +26,7 @@
 .header-container {
   position: fixed;
   top: 0;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0);
   width: 100%;
   height: 90px;
   line-height: 90px;
