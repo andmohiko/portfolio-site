bdec2b20e52cb909f56aaca0f4abc9e030cc4ea1
andmohiko <thomasito.berryz@gmail.com>
2019-12-15 03:51:32 +0900

    add portfolio workschool

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -5,11 +5,25 @@
     </div>
     <div class="portfolio-content">
       <div class="portfolio-card">
+        <a href="https://www.work-school.com/courses/5/">
+          <img src="../assets/portfolios/ws-ds-min.png" alt="workschool data scientist course" />
+        </a>
+        <h2>Workschool データサイエンティストコース</h2>
+        <p>
+          オンライン学習サービスのデータサイエンティストコースの教材を作りました。
+          <br />PythonでPandasを使ってデータを分析したりscikit-learnで機械学習してみるという内容になってます。
+        </p>
+      </div>
+
+      <div class="portfolio-card">
         <a href="http://ajdevteam.netlify.com">
           <img src="../assets/portfolios/ajdevteam-min.png" alt="ajdevteam" />
         </a>
         <h2>AJ開発チームのホームページ</h2>
-        <p>アイセックジャパンの開発チームのホームページを作りました(非公式)</p>
+        <p>
+          アイセックジャパンの開発チームのホームページを作りました(非公式)
+          <br />Bootstrapを使いました。
+        </p>
       </div>
 
       <div class="portfolio-card">
@@ -17,7 +31,10 @@
           <img src="../assets/portfolios/ps-min.png" alt="portfolio-site" />
         </a>
         <h2>ポートフォリオサイト</h2>
-        <p>自分のブログ&ポートフォリオサイトを作りました。</p>
+        <p>
+          自分のブログ&ポートフォリオサイトを作りました。
+          <br />Nuxt.js+Contentful+Netlifyを使ってます。
+        </p>
       </div>
     </div>
   </section>
@@ -65,8 +82,8 @@
 }
 
 .portfolio-card {
-  width: 400px;
-  height: 300px;
+  width: 480px;
+  height: 400px;
   background-color: rgba(0, 0, 0, 0.6);
   box-shadow: 1px 2px 3px 1px rgba(0, 0, 0, 0.2);
   border: 0.5px solid rgb(57, 72, 85);
@@ -80,8 +97,8 @@
   align-items: center;
 
   img {
-    max-height: 180px;
-    max-width: 350px;
+    max-height: 240px;
+    max-width: 360px;
     text-align: center;
     margin-bottom: 20px;
   }
@@ -93,6 +110,7 @@
   p {
     color: #e1e1e1;
     margin: 10px 0;
+    line-height: 1.2rem;
   }
 }
 </style>
