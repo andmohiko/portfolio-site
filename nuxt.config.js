3cd12e163d9d11a469049af6b801529b1a6fb73f
andmohiko <thomasito.berryz@gmail.com>
2019-12-18 02:53:22 +0900

    fix description

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'andmohiko.dev' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
