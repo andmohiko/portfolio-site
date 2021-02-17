54cd7d16a496cf4033400d11e6fbc07bb7a11bf0
andmohiko <andmohiko@gmail.com>
2021-02-17 15:27:42 +0900

    delete description

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -29,12 +29,12 @@ export default {
     return {
       title: this.post.fields.title,
       meta: [
-        { hid: 'description', name: 'description', content: this.post.fields?.description },
+        // { hid: 'description', name: 'description', content: this.post.fields?.description },
         { hid: 'og:site_name', property: 'og:site_name', content: this.post.fields.title + ' - andmohiko.devzw' },
         { hid: 'og:type', property: 'og:type', content: 'website' },
         { hid: 'og:url', property: 'og:url', content: 'https://andmohiko.dev/blog/' + this.post.fields.slug },
         { hid: 'og:title', property: 'og:title', content: this.post.fields.title },
-        { hid: 'og:description', property: 'og:description', content: this.post.fields?.description },
+        // { hid: 'og:description', property: 'og:description', content: this.post.fields?.description },
         { hid: 'og:image', property: 'og:image', content: "https:"+this.post.fields.headerImage.fields.file.url },
         { hid: 'twitter:card', name: 'twitter:card', content: "summary" },
         { hid: 'twitter:site', name: 'twitter:site', content: "@andmohiko"}
